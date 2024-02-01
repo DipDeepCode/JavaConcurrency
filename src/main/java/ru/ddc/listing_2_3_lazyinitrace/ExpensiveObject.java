c0a910466e16f596d3c7577a98610b588c1fcb68
Sergey Kovalev <dipdeepcode@mail.ru>
2024-02-01 23:17:07 +0300

    JCIP :: listing_2_3_lazyinitrace

---
+++ src/main/java/ru/ddc/listing_2_3_lazyinitrace/ExpensiveObject.java
@@ -0,0 +1,4 @@
+package ru.ddc.listing_2_3_lazyinitrace;
+
+public class ExpensiveObject {
+}
---
+++ src/main/java/ru/ddc/listing_2_3_lazyinitrace/LazyInitRace.java
@@ -0,0 +1,26 @@
+package ru.ddc.listing_2_3_lazyinitrace;
+
+/*
+Класс LazyInitRace содержит состояния гонки. Предположим, что потоки
+A и B выполняют метод getInstance в одно и то же время. A видит, что
+поле instance равно null, и создает новый ExpensiveObject. Поток B также
+проверяет, равно ли поле instance тому же значению null. Наличие в поле
+значения null в этот момент зависит от временной координации, включая
+капризы планирования и количество времени, нужного для создания экземпляра
+объекта ExpensiveObject и установки значения в поле instance.
+Если поле instance равно null, когда B его проверяет, два элемента кода,
+вызывающих метод getInstance, могут получить два разных результата,
+даже если метод getInstance предположительно должен всегда возвращать
+один и тот же экземпляр.
+*/
+
+public class LazyInitRace {
+    private ExpensiveObject instance = null;
+
+    public ExpensiveObject getInstance() {
+        if (instance == null) {
+            instance = new ExpensiveObject();
+        }
+        return instance;
+    }
+}
