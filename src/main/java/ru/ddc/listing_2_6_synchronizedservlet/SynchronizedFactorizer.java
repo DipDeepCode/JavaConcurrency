dc2bd187de1b609285b11b9318be5c0ca5afb4f0
Sergey Kovalev <dipdeepcode@mail.ru>
2024-02-06 21:02:43 +0300

    JCIP :: listing_2_6_synchronizedservlet

---
+++ src/main/java/ru/ddc/listing_2_6_synchronizedservlet/SynchronizedFactorizer.java
@@ -0,0 +1,51 @@
+package ru.ddc.listing_2_6_synchronizedservlet;
+
+import ru.ddc.tools.Servlet;
+import ru.ddc.tools.ServletRequest;
+import ru.ddc.tools.ServletResponse;
+
+import java.math.BigInteger;
+
+/*
+Механизм синхронизации позволяет легко восстановить потокобезопасность
+сервлета разложения на множители. В листинге 2.6 метод service
+делается синхронизированным, то есть впускающим один поток за раз.
+Класс SynchronizedFactorizer теперь является потокобезопасным, но
+многочисленные клиенты больше не могут использовать сервлет одновременно,
+что приводит к неприемлемо низкой отзывчивости.
+*/
+public class SynchronizedFactorizer implements Servlet {
+    private BigInteger lastNumber;
+    private BigInteger[] lastFactors;
+    @Override
+    public synchronized void service(ServletRequest req, ServletResponse resp) {
+        BigInteger i = extractFromRequest(req);
+        if (i.equals(lastNumber)) {
+            encodeIntoResponse(resp, lastFactors);
+        } else {
+            BigInteger[] factors = factor(i);
+            lastNumber = i;
+            lastFactors = factors;
+            encodeIntoResponse(resp, factors);
+        }
+    }
+
+    private BigInteger extractFromRequest(ServletRequest req) {
+        return req.getI();
+    }
+
+    private BigInteger[] factor(BigInteger n) {
+        BigInteger value = BigInteger.ONE;
+        BigInteger[] result = new BigInteger[n.intValue()];
+        result[0] = value;
+        for (int i = 2; i <= n.intValue(); i++) {
+            value = value.multiply(BigInteger.valueOf(i));
+            result[i - 1] = value;
+        }
+        return result;
+    }
+
+    private void encodeIntoResponse(ServletResponse resp, BigInteger[] factors) {
+        resp.setFactors(factors);
+    }
+}
