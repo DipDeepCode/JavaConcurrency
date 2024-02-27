388fbccdaf36da50b9aea368d7b704fd3b165bfa
Sergey Kovalev <dipdeepcode@mail.ru>
2024-02-27 22:27:10 +0300

    JCIP :: listing_4_1_simplethreadsafecounter

---
+++ src/main/java/ru/ddc/listing_4_1_simplethreadsafecounter/Counter.java
@@ -0,0 +1,29 @@
+package ru.ddc.listing_4_1_simplethreadsafecounter;
+
+import ru.ddc.annotations.GuardedBy;
+import ru.ddc.annotations.ThreadSafe;
+
+/*
+Если все поля объекта имеют примитивный тип, то можно говорить, что
+они содержат все состояние объекта. В листинге 4.1 единственное поле
+value содержит состояние счетчика. Несколько примитивных полей представляют
+собой n-элементый кортеж значений. Состоянием двумерной
+точки Point является ее значение (x, y). Состояние списка LinkedList
+включает состояния всех объектов связных узлов, входящих в список,
+если содержит поля-ссылки на эти узлы.
+*/
+@ThreadSafe
+public final class Counter {
+    @GuardedBy("this") private long value = 0;
+
+    public synchronized long getValue() {
+        return value;
+    }
+
+    public synchronized long increment() {
+        if (value == Long.MAX_VALUE) {
+            throw new IllegalArgumentException("Переполнение счетчика");
+        }
+        return ++value;
+    }
+}
