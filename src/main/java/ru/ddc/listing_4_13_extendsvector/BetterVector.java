0a3af661c0e5bc050f8ebc6e5ac3d1a88e25ba94
Sergey Kovalev <dipdeepcode@mail.ru>
2024-03-09 11:49:24 +0300

    JCIP :: listing_4_13_extendsvector

---
+++ src/main/java/ru/ddc/listing_4_13_extendsvector/BetterVector.java
@@ -0,0 +1,27 @@
+package ru.ddc.listing_4_13_extendsvector;
+
+import ru.ddc.annotations.ThreadSafe;
+
+import java.util.Vector;
+/*
+Можно расширить класс (предназначенный для расширения). Класс
+BetterVector в листинге 4.13 расширяет класс Vector, добавляя метод
+putIfAbsent.
+Но после расширения реализация политики синхронизации распределится
+по нескольким отдельно сопровождаемым исходным файлам. То
+есть, если базовый класс изменит замок для защиты своих переменных
+состояния, то подкласс станет использовать неправильный замок для
+управления конкурентным доступом к состоянию базового класса.
+(Политика синхронизации класса Vector зафиксирована в его спецификации,
+поэтому BetterVector от этой проблемы не пострадал.)
+*/
+@ThreadSafe
+public class BetterVector<E> extends Vector<E> {
+    public synchronized boolean putIfAbsent(E x) {
+        boolean absent = !contains(x);
+        if (absent) {
+            add(x);
+        }
+        return absent;
+    }
+}
